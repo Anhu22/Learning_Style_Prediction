49a9bedf9d615f3a99fa0e4ceaaac4d10ad0f912
Anhu22 <anhupamane@gmail.com>
2025-10-23 22:44:37 +0530

    "updated api.js"

--- client_front/src/api.js
+++ client_front/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Backend server URL
+  baseURL: "", // Backend server URL
   timeout: 15000,
   withCredentials: false,
 });
