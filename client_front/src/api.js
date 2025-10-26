7bf1bd281bca60fa16e265092dc2484e79b13746
Anhu22 <anhupamane@gmail.com>
2025-10-26 22:10:20 +0530

    api.js updated

--- client_front/src/api.js
+++ client_front/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "", // for Vite
+  baseURL: "", 
   // OR use process.env.REACT_APP_API_BASE_URL if using CRA
   timeout: 15000,
   withCredentials: false,
